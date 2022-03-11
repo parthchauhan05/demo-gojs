a305a50c2f02477825beb4e71f1546d5ea822b30
Parth Chauhan <parthchauhan05@gmail.com>
2022-03-10 23:03:49 -0500

    Add files via upload

--- login.js
+++ login.js
@@ -1,6 +1,5 @@
-function validate() {
-    let password = document.getElementById("_password").value;
-    if (password == "gojsdemo") {
+function validate(password) {
+    if (password == "gojs") {
         window.location.href = "home.html";
         localStorage.setItem("authenticated", new Date().getTime() + 300000);
     }
@@ -9,7 +8,6 @@ function validate() {
 function checkLogin() {
     let currentTime = new Date().getTime()
     let authentication = parseInt(localStorage.getItem("authenticated"))
-    console.log(currentTime, authentication)
     if (!authentication || currentTime > authentication) {
         window.location = "index.html";
     }
